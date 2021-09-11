63c06ee2db6982e45fd9e4b580c21021ec18b2ad
Cynthia <leewengyancynthia@gmail.com>
2021-09-11 21:54:04 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "@ionic/react/css/core.css";
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
