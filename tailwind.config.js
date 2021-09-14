70fff78ab930308e3e22759c02692affa26051cb
shiweing <shiweing1998@gmail.com>
2021-09-14 13:17:30 +0800

    Remove redundant code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ module.exports = {
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {
-            color: {
-                primary: "#5669ff",
-            },
-        },
+        extend: {},
     },
     variants: {
         extend: {},
