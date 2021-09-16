d0fc991d47877400eecd92e162d306f41ed7013c
Tian Fang <tianfangsg@gmail.com>
2021-09-16 19:22:04 +0800

    Add profile in side menu

--- src/Components/SideMenu.tsx
+++ src/Components/SideMenu.tsx
@@ -1,45 +1,49 @@
 import { menuController } from "@ionic/core";
 import {
-    IonMenu,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonList,
-    IonItem,
-    IonIcon,
-    IonLabel,
-    IonButtons,
-    IonMenuButton,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonButtons,
+  IonMenuButton,
 } from "@ionic/react";
-import { home } from "ionicons/icons";
+import { home, personCircleOutline } from "ionicons/icons";
 
 function SideMenu() {
-    return (
-        <IonMenu side="start" menuId="side-menu" contentId="main">
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Menu</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-                <IonList>
-                    <IonItem href="/home">
-                        <IonIcon icon={home} slot="start" />
-                        <IonLabel>Home</IonLabel>
-                    </IonItem>
-                </IonList>
-            </IonContent>
-        </IonMenu>
-    );
+  return (
+    <IonMenu side="start" menuId="side-menu" contentId="main">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonItem href="/home">
+            <IonIcon icon={home} slot="start" />
+            <IonLabel>Home</IonLabel>
+          </IonItem>
+          <IonItem href="/profile">
+            <IonIcon icon={personCircleOutline} slot="start" />
+            <IonLabel>Profile</IonLabel>
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
 }
 
 const openSideMenu = () => menuController.open();
 
 export const MenuButton = () => (
-    <IonButtons>
-        <IonMenuButton autoHide={false} onClick={openSideMenu} />
-    </IonButtons>
+  <IonButtons>
+    <IonMenuButton autoHide={false} onClick={openSideMenu} />
+  </IonButtons>
 );
 
 export default SideMenu;
