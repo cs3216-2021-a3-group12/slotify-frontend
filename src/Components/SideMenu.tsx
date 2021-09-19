5cc75d53c2ebe247f68c8abb4d7d3b7f4728df3b
tlylt <liu_yongliang@hotmail.com>
2021-09-19 11:57:24 +0800

    fix sidebar routing without refresh

--- src/Components/SideMenu.tsx
+++ src/Components/SideMenu.tsx
@@ -1,53 +1,60 @@
 import { menuController } from "@ionic/core";
 import {
-    IonMenu,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonList,
-    IonItem,
-    IonIcon,
-    IonLabel,
-    IonButtons,
-    IonMenuButton,
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonButtons,
+  IonMenuButton,
 } from "@ionic/react";
 import { home, personCircleOutline, searchOutline } from "ionicons/icons";
+import { Link } from "react-router-dom";
 
 function SideMenu() {
-    return (
-        <IonMenu side="start" menuId="side-menu" contentId="main">
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Menu</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-                <IonList>
-                    <IonItem href="/home">
-                        <IonIcon icon={home} slot="start" />
-                        <IonLabel>Home</IonLabel>
-                    </IonItem>
-                    <IonItem href="/explore">
-                        <IonIcon icon={searchOutline} slot="start" />
-                        <IonLabel>Explore</IonLabel>
-                    </IonItem>
-                    <IonItem href="/profile">
-                        <IonIcon icon={personCircleOutline} slot="start" />
-                        <IonLabel>Profile</IonLabel>
-                    </IonItem>
-                </IonList>
-            </IonContent>
-        </IonMenu>
-    );
+  return (
+    <IonMenu side="start" menuId="side-menu" contentId="main">
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Menu</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <Link to="/home">
+            <IonItem>
+              <IonIcon icon={home} slot="start" />
+              <IonLabel>Home</IonLabel>
+            </IonItem>
+          </Link>
+          <Link to="/explore">
+            <IonItem>
+              <IonIcon icon={searchOutline} slot="start" />
+              <IonLabel>Explore</IonLabel>
+            </IonItem>
+          </Link>
+          <Link to="/profile">
+            <IonItem>
+              <IonIcon icon={personCircleOutline} slot="start" />
+              <IonLabel>Profile</IonLabel>
+            </IonItem>
+          </Link>
+        </IonList>
+      </IonContent>
+    </IonMenu>
+  );
 }
 
 const openSideMenu = () => menuController.open();
 
 export const MenuButton = ({ ...props }) => (
-    <IonButtons {...props}>
-        <IonMenuButton autoHide={false} onClick={openSideMenu} />
-    </IonButtons>
+  <IonButtons {...props}>
+    <IonMenuButton autoHide={false} onClick={openSideMenu} />
+  </IonButtons>
 );
 
 export default SideMenu;
