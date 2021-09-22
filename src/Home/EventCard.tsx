9389bfaa25d937bf233849c8398019a08dd453a1
shiweing <shiweing1998@gmail.com>
2021-09-22 20:19:38 +0800

    Update event card

--- src/Home/EventCard.tsx
+++ src/Home/EventCard.tsx
@@ -7,28 +7,28 @@ import {
 } from "@ionic/react";
 import { timeOutline, locationOutline } from "ionicons/icons";
 import { StrippedEvent } from "../types/Event";
+import eventPlaceholder from "../resources/event-placeholder.jpg";
 
 function EventCard({ event }: { event: StrippedEvent }) {
   return (
-    <IonCard
-      className="rounded-2xl w-full h-full"
-      routerLink={`/events/${event.id}`}
-    >
-      <div className="flex flex-col items-stretch">
-        <img
-          className="p-2 w-auto object-fill rounded-2xl"
-          alt="Event"
-          src={event.image_url}
-        />
+    <IonCard className="rounded-2xl w-full h-full">
+      <div className="flex flex-col">
+        <div className="h-2/3 w-full">
+          <img
+            className="p-2 w-auto h-full object-fill rounded-2xl"
+            alt="Event"
+            src={event.image_url || eventPlaceholder}
+          />
+        </div>
         <IonCardHeader className="text-left px-2 py-0 truncate">
           <IonCardTitle className="truncate text-base">
             {event.title}
           </IonCardTitle>
-          <IonCardSubtitle className="flex items-center">
+          <IonCardSubtitle className="flex items-center m-0">
             <IonIcon icon={timeOutline} className="m-1" />
             <span>{event.end_date_time}</span>
           </IonCardSubtitle>
-          <IonCardSubtitle className="flex items-center">
+          <IonCardSubtitle className="flex items-center m-0">
             <IonIcon icon={locationOutline} className="m-1" />
             <span>{event.location}</span>
           </IonCardSubtitle>
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -44,9 +44,10 @@ function Home() {
     });
     Promise.all([
       axios_instance.get("/events/my_events"),
-      axios_instance.get("/groups/my"),
+      axios_instance.get("/groups/my_groups"),
     ]).then(([eventsRes, groupsRes]) => {
       console.log(eventsRes.data);
+      setEvents(testEvents);
       setGroups(groupsRes.data.results);
     });
   }, [userDetails.username, userDetails.accessToken]);
@@ -80,12 +81,21 @@ function Home() {
             </Link>
           </div>
           {events.length ? (
-            <IonSlides scrollbar={true} options={{ slidesPerView: "auto" }}>
+            <IonSlides
+              className="w-full"
+              scrollbar={true}
+              options={{ slidesPerView: "auto" }}
+            >
               {events.map((event) => {
                 return (
-                  <IonSlide key={event.id} className="w-2/3 h-auto mt-2 mb-4">
-                    <EventCard event={event} />
-                  </IonSlide>
+                  <Link to={`/events/${event.id}`}>
+                    <IonSlide
+                      key={event.id}
+                      className="w-full h-auto mt-2 mb-4"
+                    >
+                      <EventCard event={event} />
+                    </IonSlide>
+                  </Link>
                 );
               })}
             </IonSlides>
@@ -128,3 +138,39 @@ function Home() {
 }
 
 export default Home;
+
+const testEvents: StrippedEvent[] = [
+  {
+    id: 3,
+    title: "Public climbing session",
+    description: "we climb rocks today",
+    start_date_time: 1631608200,
+    end_date_time: 1631615400,
+    location: "Utown rock wall",
+    is_public: true,
+    group: 1,
+    image_url: null,
+  },
+  {
+    id: 2,
+    title: "Public climbing session",
+    description: "we climb rocks today",
+    start_date_time: 1631608200,
+    end_date_time: 1631615400,
+    location: "Utown rock wall",
+    is_public: true,
+    group: 1,
+    image_url: null,
+  },
+  {
+    id: 1,
+    title: "Public climbing session",
+    description: "we climb rocks today",
+    start_date_time: 1631608200,
+    end_date_time: 1631615400,
+    location: "Utown rock wall",
+    is_public: true,
+    group: 1,
+    image_url: null,
+  },
+];
