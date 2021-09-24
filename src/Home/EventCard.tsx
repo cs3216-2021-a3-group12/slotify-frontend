ae5b9c28b9cc1a07535e12ee4343335c1db087fd
shiweing <shiweing1998@gmail.com>
2021-09-24 18:37:30 +0800

    Fix Event timing bug

--- src/Home/EventCard.tsx
+++ src/Home/EventCard.tsx
@@ -2,29 +2,9 @@ import { IonCard, IonCardHeader, IonIcon, IonCardTitle } from "@ionic/react";
 import { locationOutline } from "ionicons/icons";
 import { StrippedEvent } from "../types/Event";
 import eventPlaceholder from "../resources/event-placeholder.jpg";
-import moment from "moment";
+import { getTimeDateText } from "../Event/helper";
 
 function EventCard({ event }: { event: StrippedEvent }) {
-  function getTimeDateText(event: StrippedEvent) {
-    const start = new Date(event.start_date_time);
-    const end = new Date(event.end_date_time);
-    const startMoment = moment(start);
-    const endMoment = moment(end);
-
-    if (
-      start.getDate() === end.getDate() &&
-      start.getMonth() === end.getMonth() &&
-      start.getFullYear() === end.getFullYear()
-    ) {
-      return `${startMoment.format("DD MMM")} | ${startMoment.format(
-        "H:mmA"
-      )} - ${endMoment.format("H:mmA")}`;
-    }
-    return `${startMoment.format("DD MMM H:mmA")} - ${endMoment.format(
-      "DD MMM H:mmA"
-    )}`;
-  }
-
   return (
     <IonCard className="rounded-2xl w-full h-full m-0">
       <div className="flex flex-col">
@@ -39,7 +19,9 @@ function EventCard({ event }: { event: StrippedEvent }) {
           <IonCardTitle className="truncate text-base">
             {event.title}
           </IonCardTitle>
-          <span className="my-1 text-indigo-500">{getTimeDateText(event)}</span>
+          <span className="my-1 text-indigo-500">
+            {getTimeDateText(event.start_date_time, event.end_date_time)}
+          </span>
           <div className="flex items-center m-0">
             <IonIcon icon={locationOutline} className="my-1" size="small" />
             <span>{event.location}</span>
